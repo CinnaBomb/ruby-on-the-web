12be534cf65e6c7a91e9c776be3baf7a2a663e09
Laura <Laura.Stephens@utexas.edu>
2017-01-28 00:46:56 -0600

    update readme file and switch client and browser

---
+++ README.md
@@ -0,0 +1,4 @@
+I've built a real webserver, browser and client. The browser is able to GET and POST to and from the files provided.
+
+Guidelines for project from:
+http://www.theodinproject.com/courses/ruby-programming/lessons/ruby-on-the-web
--- client.rb
+++ client.rb
@@ -1,47 +1,11 @@
-require 'socket'
-require 'json'
+require 'socket'      # Sockets are in standard library
 
-def get_post_hash
-	puts "What's your name?"
-	name = gets.chomp!
-	puts "What's your email address?"
-	email = gets.chomp!
-	post_hash = {:viking=>
-		{
-			:name =>name,
-			:email =>email
-		}
-	}
-end
-
-host = 'localhost'
+hostname = 'localhost'
 port = 2000
-get_path = "./index.html" 
-post_path = "./thanks.html"
-puts "Would you like to GET or POST"
-choice = gets.downcase.chomp!
-if choice == "get"
-	request = "GET #{get_path} HTTP/1.0\n"
-	socket = TCPSocket.open(host,port)
-	socket.puts(request) 
-	response = socket.read 
-	puts response
-	headers,body = response.split("/[\r]?[\n]+/", 2) 
-	if headers.include? "200 OK" 
-		print body
-	else
-		puts "Sorry, the file was not found."
-	end
-elsif choice =="post"
-	post_json = get_post_hash.to_json
-	request = "POST #{post_path} HTTP/1.0\nContent Length: #{post_json.length}\n#{post_json}"
-	socket = TCPSocket.open(host, port)
-	#puts request
-	socket.puts(request)
-	response = socket.read
-	puts response
 
-else
-	puts "Sorry, I didn't understand your request"
+s = TCPSocket.open(hostname, port)
 
+while line = s.gets   # Read lines from the socket
+  puts line.chop      # And print with platform line terminator
 end
+s.close               # Close the socket when done
