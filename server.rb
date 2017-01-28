8ab7e959776c4eaad20b8cfba5f6b0af55e79cd8
Laura <Laura.Stephens@utexas.edu>
2017-01-28 00:43:32 -0600

    add readme and update comments for server

--- server.rb
+++ server.rb
@@ -1,17 +1,18 @@
-require 'socket'               # Get sockets from stdlib
+require 'socket' 
 require 'json'
 
 def parse(full_request)
 	full_request = full_request.split(/[\n]+/)
-	puts full_request.inspect + "full"
-	request = full_request[0].split(" ")
-	request[2] = request[2].split("\\")[0]
+	header = full_request[0].split(" ")
+	#remove trailing newlines and carriage returns. find better way
+	header[2] = header[2].split("\\")[0]
 	request_hash = {
 		:verb => request[0],
 		:path => request[1],
 		:version => request[2]
 	}
 	if request_hash[:verb] == "POST"
+		#add extra info to request_hash
 		request_hash[:content_length] = full_request[1].match(/[\d]+/)
 		request_hash[:params] = JSON.parse(full_request[2])
 	end
@@ -26,6 +27,7 @@ def response(request_hash)
 	version = request_hash[:version]
 
 	if verb == "GET"
+		#response is standard HTTP response
 		response = "#{version} "
 		if File.exist?(path)
 			response += "200 OK\nDate: #{Time.now}\nContent-Type: text/html\n"
@@ -35,6 +37,7 @@ def response(request_hash)
 			response = "#{version} 404 Not Found"
 		end
 	elsif verb == "POST"
+		#response is path file with new info in place of yield
 		name = request_hash[:params]["viking"]["name"]
 		email = request_hash[:params]["viking"]["email"]
 		if File.exist?(path)
@@ -49,6 +52,7 @@ end
 server = TCPServer.open(2000)
 loop { 
 	client = server.accept 
+	#difficult to find a way to read from the server that doesn't close it.
 	request = client.recv(1000)
 
 	client.puts(response(parse(request)))
